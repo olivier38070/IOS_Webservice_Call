38d9e678f616fa1d1f536974d0328eb207ac3e6c
olivier <administrator@olivier.local>
2014-06-26 10:20:15 +0200

    Initial Commit

---
+++ test_webservice_call/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test_webservice_call
+//
+//  Created by olivier on 6/26/14.
+//  Copyright (c) 2014 olivier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
